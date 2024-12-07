c7d9e2e4831d308320ee3b24c5963e7e85525816
Sangram Gorai <2020wb86119@wilp.bits-pilani.ac.in>
2024-12-07 23:48:15 +0530

    update termux_awscli.sh to install AWS CLI version 2 with improved dependency handling

--- termux_awscli.sh
+++ termux_awscli.sh
@@ -2,22 +2,16 @@
 
 # Update and install dependencies
 pkg update && pkg upgrade -y
-pkg install -y curl unzip
+pkg install -y curl tar gzip
 
-# Download the AWS CLI version 2 bundle
-curl "https://awscli.amazonaws.com/awscli-exe-linux-aarch64.zip" -o "awscliv2.zip"
+# Download the AWS CLI version 2 bundle for ARM architecture
+curl "https://awscli.amazonaws.com/awscli-exe-linux-aarch64-2.2.47.zip" -o "awscliv2.zip"
 
 # Unzip the bundle
 unzip awscliv2.zip
 
-# Create necessary directories
-mkdir -p /data/data/com.termux/files/usr/lib/aws
-
-# Move AWS CLI files to the appropriate location
-mv aws/dist/* /data/data/com.termux/files/usr/lib/aws
-
-# Create a symbolic link
-ln -s /data/data/com.termux/files/usr/lib/aws/aws /data/data/com.termux/files/usr/bin/aws
+# Install the AWS CLI
+./aws/install -i /data/data/com.termux/files/usr/lib/aws -b /data/data/com.termux/files/usr/bin
 
 # Clean up
 rm -rf awscliv2.zip aws
